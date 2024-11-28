fabda26a358a7a237f3e4d83f3dd61a587b96680
Yechiel Babani <yechielb2000@gmail.com>
2024-11-28 13:39:09 +0200

    add comment for redis config file

--- services/redis/build.sh
+++ services/redis/build.sh
@@ -1,5 +1,6 @@
 
 #TODO: use my own address and port on prod
+# -v $(pwd)/redis.conf:/usr/local/etc/redis/redis.conf (to set redis config file from my file)
 
 docker pull redis/redis-stack-server:latest
 docker run -d --name redis-stack-server -p 6379:6379 redis/redis-stack-server:latest
