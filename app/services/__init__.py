714fa33f5dcacf968c520250dc98392b3ad66c24
Yechiel Babani <yechielb2000@gmail.com>
2024-11-28 13:56:02 +0200

    remove dataclass

---
+++ app/services/__init__.py
@@ -0,0 +1,5 @@
+from app.services.redis_conf import redis_client
+
+__all__ = [
+    'redis_client'
+]
--- app/services/redis_conf.py
+++ app/services/redis_conf.py
@@ -1,18 +1,10 @@
 import os
-from dataclasses import dataclass
 
 from redis import Redis
 
-
-@dataclass
-class RedisConfig:
-    host: str = os.environ.get('REDIS_ADDRESS', '0.0.0.0')
-    port: int = os.environ.get('REDIS_PORT', 6379)
-
-
 redis_client = Redis(
-    host=RedisConfig.host,
-    port=RedisConfig.port,
+    host=os.environ.get('REDIS_ADDRESS', '0.0.0.0'),
+    port=os.environ.get('REDIS_PORT', 6379),
     decode_responses=True,
     ssl=True
 )
